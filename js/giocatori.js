da3a99ebdb7e7e7ec93157b14dbeb0ec2d7d5adc
Gabriele Fronzé <sucre.91@hotmail.it>
2022-02-24 19:13:31 +0100

    Implementing automatic score parsing

--- js/giocatori.js
+++ js/giocatori.js
@@ -1,9 +1,18 @@
 var select = document.getElementById("giocatori");
 
-for(var i = 0; i < options.length; i++) {
-    var opt = options[i];
+for(var i = 0; i < giocatori.length; i++) {
+    var opt = giocatori[i];
     var el = document.createElement("option");
-    el.textContent = opt[0];
+    el.textContent = opt[1];
     el.value = opt[0];
     select.appendChild(el);
+}
+
+function update_punteggi(caller){
+    console.log(caller.value)
+    var found = giocatori.filter((g) => g[0] === caller.value)
+    console.log(found)
+    var score = document.getElementById(caller.id+"_score")
+    score.value = (found.length !== 0)?found[0][1]:0
+    fanfani()
 }
